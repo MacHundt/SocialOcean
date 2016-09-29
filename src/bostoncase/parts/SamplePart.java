b0a948b8826dbc0f43f9edf0b66d6c34a1e5f9b1
Michael Hundt <michael.hundt@uni-konstanz.de>
2016-09-29 17:39:49 +0200

    update view

--- src/bostoncase/parts/SamplePart.java
+++ src/bostoncase/parts/SamplePart.java
@@ -1,7 +1,9 @@
 package bostoncase.parts;
 
+import java.awt.BorderLayout;
 import java.util.Arrays;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
@@ -11,17 +13,26 @@ import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
+
+import widget.MapWidget;
 
 public class SamplePart {
 
 	private Text txtInput;
 	private TableViewer tableViewer;
+	private MapWidget mapvis;
+	private Tree layerTree;
 
 	@Inject
 	private MDirtyable dirty;
@@ -39,12 +50,45 @@ public class SamplePart {
 			}
 		});
 		txtInput.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
-
+		
+//		SashForm sashForm = new SashForm(parent, SWT.VERTICAL);
+//		sashForm.setLayoutData(BorderLayout.CENTER);
+//		
+//		SashForm middle = new SashForm(sashForm, SWT.HORIZONTAL);
+//		
+//		Group grpLayers = new Group(middle, SWT.NONE);
+//		grpLayers.setText("Layers");
+//		grpLayers.setLayout(new FillLayout(SWT.HORIZONTAL));
+//		
+//		layerTree = new Tree(grpLayers, SWT.BORDER);
+//		
+//		TreeItem trtmBaseMap = new TreeItem(layerTree, SWT.NONE);
+//		trtmBaseMap.setText("BaseMap");
+//		
+//		TreeItem trtmMapnic = new TreeItem(trtmBaseMap, SWT.NONE);
+//		trtmMapnic.setText("OSM-Mapnic");
+//		trtmBaseMap.setExpanded(true);
+//		
+//		TreeItem trtmTweets = new TreeItem(layerTree, SWT.NONE);
+//		trtmTweets.setText("Tweets");
+//		
+//		
+//		mapvis = new MapWidget(middle, SWT.NONE);
+//		
+//		Group grpSettings = new Group(middle, SWT.NONE);
+//		grpSettings.setText("Settings");
+//		middle.setWeights(new int[] {109, 258, 77});
+		
+		
 		tableViewer = new TableViewer(parent);
 
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());;
 		tableViewer.setInput(createInitialDataModel());
 		tableViewer.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		
+		mapvis = new MapWidget(parent, SWT.CENTER);
+		mapvis.setLayoutData(new GridData(GridData.FILL_BOTH));
+		
 	}
 
 	@Focus
@@ -58,6 +102,6 @@ public class SamplePart {
 	}
 	
 	private List<String> createInitialDataModel() {
-		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5");
+		return Arrays.asList("Sample item 1", "Sample item 2", "Sample item 3", "Sample item 4", "Sample item 5", "Sample item 6");
 	}
 }
