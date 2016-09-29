1856e9b17bbaac22abbf4418f1a70cd00b6e4e27
Michael Hundt <michael.hundt@uni-konstanz.de>
2016-09-29 17:04:18 +0200

    commit project

--- src/bostoncase/handlers/AboutHandler.java
+++ src/bostoncase/handlers/AboutHandler.java
@@ -7,6 +7,6 @@ import org.eclipse.swt.widgets.Shell;
 public class AboutHandler {
 	@Execute
 	public void execute(Shell shell) {
-		MessageDialog.openInformation(shell, "About", "BostonCase");
+		MessageDialog.openInformation(shell, "About", "Eclipse 4 RCP Application");
 	}
 }
