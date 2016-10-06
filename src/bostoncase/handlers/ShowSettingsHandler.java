c4dc4d0591a6e7e486fc5810fc307c157a76313f
Michael Hundt <michael.hundt@uni-konstanz.de>
2016-10-06 17:38:41 +0200

    include fragments :)

--- src/bostoncase/handlers/ShowSettingsHandler.java
+++ src/bostoncase/handlers/ShowSettingsHandler.java
@@ -5,9 +5,15 @@ import org.eclipse.e4.core.di.annotations.Execute;
 
 public class ShowSettingsHandler {
 	
+	
 	@Execute
 	public void execute() {
 		System.out.println("show Settings");
+		
+//		console.outputConsole("Show Settings");
+		
 	}
+	
+	
 		
 }
--- src/bostoncase/parts/Console.java
+++ src/bostoncase/parts/Console.java
@@ -2,8 +2,12 @@ package bostoncase.parts;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
@@ -30,6 +34,19 @@ public class Console {
 	@Focus
 	public void onFocus() {
 		console.setFocus();
+	}
+	
+	
+	@Inject
+	@Optional
+	public void receiveActivePart(
+	        @Named(IServiceConstants.ACTIVE_PART) MPart activePart) {
+	        if (activePart != null) {
+	                System.out.println("Active part changed "
+	                                + activePart.getLabel());
+	                outputConsole("Active part changed "
+	                                + activePart.getLabel());
+	        }
 	}
 	
 	
